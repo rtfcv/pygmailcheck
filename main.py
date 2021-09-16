36d7d2b1ac743a93bcca5d1e17ef190cff2d8a2b
ryo <example.483@gmail.com>
2021-09-16 15:02:27 +0900

    added the most basic multiuser capabilities

--- main.py
+++ main.py
@@ -22,99 +22,98 @@ def main():
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
     """
-    creds = None
-
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-
-    # Later create multiple files for token                      ##############
-    # create add_user switch
-    # create
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
     # user = 'example'
     # if os.path.exists(+'example/token.json'):
     #     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # service = {}
-    # users = ['one', 'two']
+    service = {}
+    users = ['one', 'two']
 
-    # for user in users:
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json',
-                SCOPES
-            )
-            creds = flow.run_local_server(port=0)
+    for user in users:
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        creds = None
 
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    # Later create multiple files for token                      ##############
+        # create add_user switch
+        if os.path.exists(user+'token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json',
+                    SCOPES
+                )
+                creds = flow.run_local_server(port=0)
 
-    # instanciate stuffs
-    service = build('gmail', 'v1', credentials=creds)
+            # Save the credentials for the next run
+            with open(user+'token.json', 'w') as token:
+                token.write(creds.to_json())
+        # Later create multiple files for token                      ##############
+
+        # instanciate stuffs
+        service[user] = build('gmail', 'v1', credentials=creds)
 
     notify = pyNotify.PyNotify()
 
-    oldDF = None
-    newDF = None
+    oldDF = {}
+    newDF = {}
 
     # MAIN LOOP
     while True:
-        try:
-            results = service.users().messages().list(userId='me').execute()
-        except BaseException as e:
-            print(e)
-            service = build('gmail', 'v1', credentials=creds)
-            results = service.users().messages().list(userId='me').execute()
-
-        mesgs = results.get('messages', [])
-        newDF = pd.DataFrame(mesgs).set_index('id', drop=True)
-
-        if oldDF is None:
-            oldDF = newDF
-        newMail = newDF[~newDF.isin(oldDF)].dropna()
-
-        for _id in newMail.index:
-            txt = service.users().messages().get(userId='me', id=_id).execute()
-
-            # Use try-except to avoid any Errors
+        for user in users:
             try:
-                # Get value of 'payload' from dictionary 'txt'
-                payload = txt['payload']
-                headers = payload['headers']
+                results = service[user].users().messages().list(userId='me').execute()
             except BaseException as e:
                 print(e)
-                continue
+                service = build('gmail', 'v1', credentials=creds)
+                results = service[user].users().messages().list(userId='me').execute()
 
-            try:
-                # Look for Subject and Sender Email in the headers
-                for d in headers:
-                    if d['name'] == 'Subject':
-                        subject = d['value']
-                    if d['name'] == 'From':
-                        sender = d['value']
+            mesgs = results.get('messages', [])
+            newDF[user] = pd.DataFrame(mesgs).set_index('id', drop=True)
 
-                # Printing the subject, sender's email and message
-                print(f'Subject: {subject}')
-                print(f'From: {sender}')
-                print('\n')
+            if not (user in oldDF.keys()):
+                oldDF[user] = newDF[user]
+            newMail = newDF[user][~newDF[user].isin(oldDF[user])].dropna()
 
-                notify.notify('gmail', sender, subject)
+            for _id in newMail.index:
+                txt = service[user].users().messages().get(userId='me', id=_id).execute()
 
-            except BaseException as e:
-                print(e)
-                pass
+                # Use try-except to avoid any Errors
+                try:
+                    # Get value of 'payload' from dictionary 'txt'
+                    payload = txt['payload']
+                    headers = payload['headers']
+                except BaseException as e:
+                    print(e)
+                    continue
 
-        oldDF = newDF
+                try:
+                    # Look for Subject and Sender Email in the headers
+                    for d in headers:
+                        if d['name'] == 'Subject':
+                            subject = d['value']
+                        if d['name'] == 'From':
+                            sender = d['value']
 
-        # repeat the above process for multiple users
+                    # Printing the subject, sender's email and message
+                    print(f'User: {user}')
+                    print(f'Subject: {subject}')
+                    print(f'From: {sender}')
+                    print('\n')
+
+                    notify.notify('gmail', user, sender + '\n' +subject)
+
+                except BaseException as e:
+                    print(e)
+                    pass
+
+            oldDF[user] = newDF[user]
+
+            # repeat the above process for multiple users
 
         try:
             time.sleep(300)
